fd22544e620c6ee95cc793dbac839d782554b156
23027275 <163567157+23027275@users.noreply.github.com>
2024-03-15 20:56:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     double merchandiseCost, employeesSalary, rent, electricityCost, desiredProfit;
 
-    // My Input data
+   
     cout << "Enter the total cost of merchandise: ";
     cin >> merchandiseCost;
 
@@ -20,21 +20,20 @@ int main() {
     cout << "Enter the desired net profit percentage (e.g., 10 for 10%): ";
     cin >> desiredProfit;
 
-    // all my Calculations
+
     double totalExpenses = merchandiseCost + employeesSalary + rent + electricityCost;
     double desiredProfitAmount = (desiredProfit / 100) * merchandiseCost;
     double totalRevenueNeeded = totalExpenses + desiredProfitAmount;
 
-    // The application of the sale discount
+    
     double saleDiscount = 0.15; // 15% sale discount
     totalRevenueNeeded /= (1 - saleDiscount);
 
-    //My Calculating markup percentage
     double markupPercentage = (totalRevenueNeeded / merchandiseCost - 1) * 100;
 
-    // Finding Output
+
     cout << "To achieve a net profit of approximately " << desiredProfit << "%, ";
     cout << "the merchandise should be marked up by approximately " << markupPercentage << "%." << endl;
 
-    return 0;
+Â Â Â Â returnÂ 0;
 }
